6b029311789c2a710f841268149041fffe184ca6
Marvin <90805899+Shy-and-sheep@users.noreply.github.com>
2024-03-22 16:02:14 +0100

    Intégration de la navbar à ProfileView et PlanningPersonnelView + Ajout d'infos à ProfilView

--- MobileEM.swiftpm/Views/PlanningPersonnel.swift
+++ MobileEM.swiftpm/Views/PlanningPersonnel.swift
@@ -6,6 +6,8 @@ struct PlanningPersonnelView: View {
     // TEST TO REMOVE
     @ObservedObject var planningVM : PlanningViewModel
     
+    let benevoleVM: BenevoleViewModel
+    
     let pseudo : String
     
     var samediInscriptions: [InscriptionWithAllInfosDTO] {
@@ -25,133 +27,175 @@ struct PlanningPersonnelView: View {
         
         // TEST TO REMOVE
         let planningIntent = PlanningIntent(planningViewModel: planningVM)
-        
-        VStack{
-            HStack{
-                Text("Planning Personnel")
-                    .font(.title)
-                    .padding(40)
-            }
-            ScrollView{
-                Text("Samedi")
-                    .font(.title3)
-                ForEach(samediInscriptions, id: \.self) { inscription in
+        NavigationView{
+            VStack{
+                VStack{
                     HStack{
-                        Text("\(inscription.creneauDto.heureDebut) - \(inscription.creneauDto.heureFin)")
-                                .padding(10)
-                        Divider()
-                        Text(inscription.espaceDto.libelleEspace)
-                        
-                        Spacer()
-                        Button(action: {
-                            print("Supprime")
-                        }){
-                            Image(systemName: "trash")
-                        }
-                        .padding(10)
-                    }                  
-                    Divider()
-                }
-                Spacer(minLength: 70)
-                Text("Dimanche")
-                    .font(.title3)
-                ForEach(dimancheInscriptions, id: \.self) {inscription in
-                    HStack{
-                        Text("\(inscription.creneauDto.heureDebut) - \(inscription.creneauDto.heureFin)")
-                                .padding(10)
-                        Divider()
-                        Text(inscription.espaceDto.libelleEspace)
-                        
-                        Spacer()
-                        Button(action: {
-                            print("Supprime")
-                        }){
-                            Image(systemName: "trash")
-                        }
-                        .padding(10)
+                        Text("Planning Personnel")
+                            .font(.title)
+                            .padding(40)
                     }
-                    Divider()
+                    ScrollView{
+                        Text("Samedi")
+                            .font(.title3)
+                        ForEach(samediInscriptions, id: \.self) { inscription in
+                            HStack{
+                                Text("\(inscription.creneauDto.heureDebut) - \(inscription.creneauDto.heureFin)")
+                                    .padding(10)
+                                Divider()
+                                Text(inscription.espaceDto.libelleEspace)
+                                
+                                Spacer()
+                                Button(action: {
+                                    print("Supprime")
+                                }){
+                                    Image(systemName: "trash")
+                                }
+                                .padding(10)
+                            }                  
+                            Divider()
+                        }
+                        Spacer(minLength: 70)
+                        Text("Dimanche")
+                            .font(.title3)
+                        ForEach(dimancheInscriptions, id: \.self) {inscription in
+                            HStack{
+                                Text("\(inscription.creneauDto.heureDebut) - \(inscription.creneauDto.heureFin)")
+                                    .padding(10)
+                                Divider()
+                                Text(inscription.espaceDto.libelleEspace)
+                                
+                                Spacer()
+                                Button(action: {
+                                    print("Supprime")
+                                }){
+                                    Image(systemName: "trash")
+                                }
+                                .padding(10)
+                            }
+                            Divider()
+                        }
+                    }
                 }
+                .onAppear {
+                    // Call your function here
+                    Task {
+                        // Perform your setup tasks or fetch data here
+                        debugPrint("Fetching data...")
+                        
+                        var result = await inscriptionPosteIntent.getInscriptionsByPseudo(pseudo: pseudo)
+                        
+                        // TEST TO REMOVE
+                        var result2 = await planningIntent.getPlanning(idF: 2)
+                        
+                        if (result)
+                        {
+                            //debugPrint("Wihii")
+                            //debugPrint(sinscrireVM.inscriptions.count)
+                            //for inscription in sinscrireVM.inscriptions
+                            //{
+                            //    debugPrint("=====================")
+                            //    debugPrint(inscription)
+                            //    debugPrint("=====================")
+                            //}
+                        }
+                        else
+                        {
+                            debugPrint(":c")
+                        }
+                        
+                        // TEST TO REMOVE
+                        if (result2)
+                        {
+                            debugPrint("Planning :")
+                            for inscription in planningVM.inscriptions
+                            {
+                                debugPrint("=====INSCRIPTIONS=====")
+                                debugPrint(inscription)
+                                debugPrint("=====================")
+                            }
+                            
+                            for poste in planningVM.postes
+                            {
+                                debugPrint("=====POSTES=====")
+                                debugPrint(poste)
+                                debugPrint("=====================")
+                            }
+                            
+                            for creneau in planningVM.creneaux
+                            {
+                                debugPrint("=====CRENEAUX=====")
+                                debugPrint(creneau)
+                                debugPrint("=====================")
+                            }
+                            
+                            for espace in planningVM.espaces
+                            {
+                                debugPrint("=====ESPACES=====")
+                                debugPrint(espace)
+                                debugPrint("=====================")
+                            }
+                            
+                            for nbPlace in planningVM.nombrePlaceTotal
+                            {
+                                debugPrint("=====NB PLACE=====")
+                                debugPrint("\(nbPlace.key) -> \(nbPlace.value)")
+                                debugPrint("=====================")
+                            }
+                        }
+                        else
+                        {
+                            debugPrint(">:(")
+                        }
+                    }
+                }
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    
+                    NavigationLink(destination: ProfileView(benevoleVM: benevoleVM, pseudo: pseudo, sinscrireVM: sinscrireVM, planningVM: planningVM)){
+                        Image(systemName: "person")
+                    }
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: PlanningPersonnelView(sinscrireVM: sinscrireVM, planningVM: planningVM, benevoleVM: benevoleVM, pseudo: pseudo)){
+                        Image(systemName: "calendar")
+                    }
+                    
+                    Spacer()
+                    
+                    /*
+                    NavigationLink(destination: InscriptionSamediView()){
+                        Image(systemName: "list.bullet.clipboard")
+                    }
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: ProfilView()){
+                        Image(systemName: "hourglass")
+                    }
+                    Spacer()
+                    
+                    
+                     NavigationLink(destination: FithView()){
+                     Image(systemName: "info.circle")
+                     }
+                     Spacer()
+                     */
+                }
+                .padding()
+                .foregroundColor(.blue)
             }
         }
-        .onAppear {
-            // Call your function here
-            Task {
-                // Perform your setup tasks or fetch data here
-                debugPrint("Fetching data...")
-                
-                var result = await inscriptionPosteIntent.getInscriptionsByPseudo(pseudo: pseudo)
-                
-                // TEST TO REMOVE
-                var result2 = await planningIntent.getPlanning(idF: 2)
-                
-                if (result)
-                {
-                    //debugPrint("Wihii")
-                    //debugPrint(sinscrireVM.inscriptions.count)
-                    //for inscription in sinscrireVM.inscriptions
-                    //{
-                    //    debugPrint("=====================")
-                    //    debugPrint(inscription)
-                    //    debugPrint("=====================")
-                    //}
-                }
-                else
-                {
-                    debugPrint(":c")
-                }
-                
-                // TEST TO REMOVE
-                if (result2)
-                {
-                    debugPrint("Planning :")
-                    for inscription in planningVM.inscriptions
-                    {
-                        debugPrint("=====INSCRIPTIONS=====")
-                        debugPrint(inscription)
-                        debugPrint("=====================")
-                    }
-                    
-                    for poste in planningVM.postes
-                    {
-                        debugPrint("=====POSTES=====")
-                        debugPrint(poste)
-                        debugPrint("=====================")
-                    }
-                    
-                    for creneau in planningVM.creneaux
-                    {
-                        debugPrint("=====CRENEAUX=====")
-                        debugPrint(creneau)
-                        debugPrint("=====================")
-                    }
-                    
-                    for espace in planningVM.espaces
-                    {
-                        debugPrint("=====ESPACES=====")
-                        debugPrint(espace)
-                        debugPrint("=====================")
-                    }
-                    
-                    for nbPlace in planningVM.nombrePlaceTotal
-                    {
-                        debugPrint("=====NB PLACE=====")
-                        debugPrint("\(nbPlace.key) -> \(nbPlace.value)")
-                        debugPrint("=====================")
-                    }
-                }
-                else
-                {
-                 debugPrint(">:(")
-                }
-            }
-        }
+        .navigationBarHidden(true)
     }
 }
 
 
 struct PlanningPersonnelView_Previews: PreviewProvider {
     static var previews: some View {
-        PlanningPersonnelView(sinscrireVM: SinscrireViewModel(inscriptionDTOs: []), planningVM: PlanningViewModel(), pseudo: "blipbloup")
+        PlanningPersonnelView(sinscrireVM: SinscrireViewModel(inscriptionDTOs: []), planningVM: PlanningViewModel(), benevoleVM: BenevoleViewModel(), pseudo: "blipbloup")
     }
 }
--- MobileEM.swiftpm/Views/ProfileView.swift
+++ MobileEM.swiftpm/Views/ProfileView.swift
@@ -11,27 +11,114 @@ import SwiftUI
 struct ProfileView: View {
     let benevoleVM: BenevoleViewModel
     
+    let pseudo : String
+    
+    @ObservedObject var sinscrireVM : SinscrireViewModel
+    
+    // TEST TO REMOVE
+    @ObservedObject var planningVM : PlanningViewModel
+    
     var body: some View {
-        VStack {
-            Text("Profil de \(benevoleVM.pseudo)")
-                .font(.title)
-                .padding()
-            
-            Text("Nom: \(benevoleVM.nom)")
-            Text("Prenom: \(benevoleVM.prenom)")
-            Text("Email: \(benevoleVM.email)")
-            Text("Pseudo: \(benevoleVM.pseudo)")
-            Text("téléphone: \(benevoleVM.numTel)")
+        NavigationView{
+            VStack {
+                Text("Profil de \(benevoleVM.pseudo)")
+                    .font(.title)
+                    .padding(40)
                 
-            
+                VStack(alignment: .leading){
+                    
+                    Text("Nom : \(benevoleVM.nom)")
+                        .padding(10)
+                    
+                    Text("Prénom : \(benevoleVM.prenom)")
+                        .padding(10)
+                   
+                    Text("Email : \(benevoleVM.email)")
+                        .padding(10)
+                    
+                    Text("Pseudo : \(benevoleVM.pseudo)")
+                        .padding(10)
+                    
+                    Text("Numéro de téléphone : \(benevoleVM.numTel)")
+                        .padding(10)
+                    
+                    Text("Taille du t-shirt : \(benevoleVM.taille)")
+                        .padding(10)
+                    
+                    if(benevoleVM.vegetarien == true){
+                        Text("Végétarien : Oui")
+                            .padding(10)
+                    } else {
+                        Text("Végétarien : Non")
+                            .padding(10)
+                    }
+                    
+                    if(benevoleVM.chercheLogement == true){
+                        Text("Besoin d'un logement : Oui")
+                            .padding(10)
+                    } else {
+                        Text("Besoin d'un logement : Non")
+                            .padding(10)
+                    }
+                }
+                Spacer()
+                // Remplacer le bouton par une NavigationLink
+                Button(action: {
+                    print("modification")
+                }, label: {
+                    Text("Modifier Profil")
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                })
+                Spacer()
+                HStack {
+                    Spacer()
+                    
+                    NavigationLink(destination: ProfileView(benevoleVM: benevoleVM, pseudo: pseudo, sinscrireVM: sinscrireVM, planningVM: planningVM)){
+                        Image(systemName: "person")
+                    }
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: PlanningPersonnelView(sinscrireVM: sinscrireVM, planningVM: planningVM, benevoleVM: benevoleVM, pseudo: pseudo)){
+                        Image(systemName: "calendar")
+                    }
+                    
+                    Spacer()
+                    
+                    /*
+                     NavigationLink(destination: InscriptionSamediView()){
+                     Image(systemName: "list.bullet.clipboard")
+                     }
+                     
+                     Spacer()
+                     
+                     NavigationLink(destination: ProfilView()){
+                     Image(systemName: "hourglass")
+                     }
+                     Spacer()
+                     
+                     
+                     NavigationLink(destination: FithView()){
+                     Image(systemName: "info.circle")
+                     }
+                     Spacer()
+                     */
+                }
+                .padding()
+                .foregroundColor(.blue)
+            }
         }
+        .navigationBarHidden(true)
     }
 }
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         let user = BenevoleViewModel()
-        return ProfileView(benevoleVM: user)
+        return ProfileView(benevoleVM: user, pseudo: "blipbloup", sinscrireVM: SinscrireViewModel(inscriptionDTOs: []), planningVM: PlanningViewModel())
     }
 }
 
